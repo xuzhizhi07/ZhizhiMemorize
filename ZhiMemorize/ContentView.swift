e5f3a5fa08554f19a67bcbd8360b7dc4b3dbc3d9
xuzhizhi07 <1015373877@qq.com>
2025-02-24 17:24:17 +0800

    Initial Commit

---
+++ ZhiMemorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZhiMemorize
+//
+//  Created by 抱谷 on 2025/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZhiMemorize/ZhiMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhiMemorizeApp.swift
+//  ZhiMemorize
+//
+//  Created by 抱谷 on 2025/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ZhiMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
